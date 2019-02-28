bc1560ece3a4f10104ba4bc911d556c27d83e8b1
igor_yazovcev <uSAg9wbxUX6NEROI>
2019-02-28 06:21:35 +0500

    Delete unused imports

--- app/src/main/java/ru/gc986/testLogs/MainActivity.kt
+++ app/src/main/java/ru/gc986/testLogs/MainActivity.kt
@@ -1,9 +1,7 @@
 package ru.gc986.testLogs
 
-import android.app.Application
-import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.View.VISIBLE
 import kotlinx.android.synthetic.main.activity_main.*
